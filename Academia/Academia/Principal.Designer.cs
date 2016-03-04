c823711ede771ac461b2dafb88de86d30c93f7b8
Joham Nunes <johamnunes@gmail.com>
2016-03-04 11:39:25 -0300

    Edição Tela Principal

--- Academia/Academia/Principal.Designer.cs
+++ Academia/Academia/Principal.Designer.cs
@@ -28,9 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1008, 661);
+            this.Name = "Principal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Academia";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
