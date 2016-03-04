647cd0bd47f53825bd9064eff3ba42d8cb6e4e3b
Gabriel28Ruas <gabrielbruas19@gmail.com>
2016-03-04 12:30:22 -0300

    Criação dos modelos dos Bancos de dados

---
+++ Academia/Academia/Models/Aluno.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Academia.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Apelido { get; set; }
+        public string Logradouro { get; set; }
+        public int Numero { get; set; }
+        public string Bairro { get; set; }
+        public string Complmento { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public string Telefone { get; set; }
+        public string TelEmergencia { get; set; }
+        public string Sexo { get; set; }
+        public DateTime DataNasc { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public string Obs { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ Academia/Academia/Models/Atividade.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Academia.Models
+{
+    public class Atividade
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public decimal Valor { get; set; }
+        public DateTime CreatedAT { get; set; }
+    }
+}
